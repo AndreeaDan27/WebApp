0928cf79341327711d64ffa2f4d715e328ac0121
AndreeaDan27 <andan@microsoft.com>
2022-12-15 09:35:00 +0100

    added frontend

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # This web app defines a single route, /, which returns the string "Hello, World!" when accessed.
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, from PowerBI!'
+def hello_world():
+    return render_template('front/index.html')
 
 if __name__ == '__main__':
     app.run()
